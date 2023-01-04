0df2c2944550e2f56aedd9f9d4b1ec6431d93597
Wanlin Li <wanlinli2018@gmail.com>
2023-01-03 19:00:04 -0500

    RaxML+RF

--- workflow/scripts/RaxFilterBootstrap.py
+++ workflow/scripts/RaxFilterBootstrap.py
@@ -10,9 +10,6 @@ def calculateAverageBootstrapRax(outtree):
 
 def filterBootstrap(raxmlTree,Bootstrap_file, bootstrap_threshold=0):
     bootstrap = calculateAverageBootstrapRax(raxmlTree)
-   # print("bootstrap_threshold: ", bootstrap_threshold)
-   # print("bootstrap: ",bootstrap)
-     #filter
     if bootstrap >= float(bootstrap_threshold):
         with open(Bootstrap_file,'w') as f:
             f.write(str(bootstrap))
--- workflow/scripts/RaxmlFilterRf.py
+++ workflow/scripts/RaxmlFilterRf.py
@@ -4,25 +4,22 @@ from csv import writer
 
 def addToCsv(window_pos, ref_feature, bootstrap_average, normalized_RF,output_csv):
     list = [window_pos, ref_feature, bootstrap_average, normalized_RF]
-    #with open('output/filteredWindows'+ time.strftime("%Y%m%d-%H%M") + '.csv', 'a') as f_object:
     with open(output_csv, 'a') as f_object:
         writer_object = writer(f_object)
         writer_object.writerow(list)
         f_object.close()
 
-def rfFilter(file_seqTree,file_refTree,file_bootstrap_value,ete3_output,rf_threshold =100):
+def rfFilter(file_seqTree,file_refTree,file_bootstrap_value,ete3_output,window_pos,ref_feature,rf_threshold =100):
     if os.stat(file_bootstrap_value).st_size == 0:   #if file_seqTree is empty, it means bootstrap < threshold
         open(ete3_output, 'w').close()    # create an empty file
     else:
-        #os.system("ete3 compare -t " + file_seqTree + " -r " + file_refTree + " --unrooted >" + ete3_output)
         os.system("rf " + file_seqTree + " " + file_refTree + " > " + ete3_output)
         with open(ete3_output, 'r') as file:
             normalized_rf = file.read().rstrip()
         
         if float(normalized_rf)*100 <= float(rf_threshold):
-            with open("results/windows/rf_filtered.txt", "a") as f:
-                window_pos = file_seqTree[14:-15]
-                ref_feature = file_refTree[16:-7]
+            output_csv = "results/output_raxml.csv"
+            with open(output_csv, "a") as f:
                 normalized_RF = round(float(normalized_rf)*100,2)
                 with open(file_bootstrap_value, 'r') as file:
                     bootstrap_average = round(float(file.read().rstrip()),2)
@@ -30,4 +27,4 @@ def rfFilter(file_seqTree,file_refTree,file_bootstrap_value,ete3_output,rf_thres
                 
 
 if __name__ == '__main__':
-    rfFilter(snakemake.input.seq_tree,snakemake.input.ref_tree,snakemake.input.bootstrap_value,snakemake.output.ete3_output, snakemake.config['Thresholds']['rf_threshold'])
+    rfFilter(snakemake.input.seq_tree,snakemake.input.ref_tree,snakemake.input.bootstrap_value,snakemake.output.ete3_output,snakemake.wildcards.windows_pos,snakemake.wildcards.feature, snakemake.config['Thresholds']['rf_threshold'])
