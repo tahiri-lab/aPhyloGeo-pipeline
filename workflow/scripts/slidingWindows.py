cd242500a1659821c5be7e140a78918a74548df5
Nadia Tahiri, PhD <Nadia.Tahiri@USherbrooke.ca>
2023-01-30 15:57:45 -0500

    formatting

--- workflow/scripts/slidingWindows.py
+++ workflow/scripts/slidingWindows.py
@@ -1,6 +1,5 @@
 from Bio import SeqIO
 
-
 def slidingWindows(alignment_output, window_pos, outfile_name):
     for seq_record in SeqIO.parse(alignment_output, "fasta"):
         start, end = window_pos.split('_')
@@ -9,4 +8,8 @@ def slidingWindows(alignment_output, window_pos, outfile_name):
             SeqIO.write(records, handle, "fasta") 
 
 if __name__ == '__main__':
-    slidingWindows(snakemake.params.alignment_output, snakemake.wildcards.position, snakemake.output.windows)
+    slidingWindows(
+        snakemake.params.alignment_output,
+        snakemake.wildcards.position,
+        snakemake.output.windows
+    )
