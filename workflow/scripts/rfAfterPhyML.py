1fa44f890f024164a09c0904d52aa0b028a600ff
Wanlin Li <wanlinli2018@gmail.com>
2023-02-06 16:13:08 -0500

    test

--- workflow/scripts/rfAfterPhyML.py
+++ workflow/scripts/rfAfterPhyML.py
@@ -12,7 +12,7 @@ def addToCsv(window_pos, ref_feature, normalized_RF,output_csv):
 
 
 def filter_RF(rf_outfile,output_csv):
-    time.sleep(5)
+    #time.sleep(1)
     columns_name = ["window_pos", "ref_feature","normalized_RF"]
     if not os.path.exists(output_csv):
         with open(output_csv, 'w') as f_object:
